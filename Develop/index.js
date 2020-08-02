836443a1d84c060ecdaf7cb36d98bda5f8808a65
Hannah Jenkins <hannahbrittany@hotmail.com>
2020-08-02 19:13:26 -0400

    added prompt questions using inquirer

--- Develop/index.js
+++ Develop/index.js
@@ -1,16 +1,58 @@
+// require inquirer 
+const inquirer = require('inquirer');
+const Choice = require('inquirer/lib/objects/choice');
+
 // array of questions for user
-const questions = [
 
-];
+inquirer.prompt([
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is the title of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please enter a description of your project:'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Please enter installation instructions for your project:'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please provide examples and instructions for use:'
+    },
+    {
+        type: 'input',
+        name: 'credits',
+        message: 'List your collaborators, if any, with links to their GitHub profiles.'
+    },
+    {
+        type: 'checkbox',
+        name: 'license',
+        message: 'Please select a license:',
+        choices: ['MIT', 'Apache', 'ISC', 'GNU AGPLv3', 'GNU GPLv3', 'Mozilla Public License', 'The Unlicense']
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Provide tests and give examples on how to use them:'
+    }
 
-// function to write README file
-function writeToFile(fileName, data) {
-}
+])
+.then(answers => console.log(answers));
 
-// function to initialize program
-function init() {
+// // function to write README file
+// function writeToFile(fileName, data) {
+// }
 
-}
+// // function to initialize program
+// function init() {
 
-// function call to initialize program
-init();
+// }
+
+// // function call to initialize program
+// init();
